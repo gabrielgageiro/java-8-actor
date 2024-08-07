387965b9d07a7ef524210767f30adfeb0c714a2c
gabrielgageiro <gabrielgageiro@gmail.com>
2024-08-07 17:49:45 -0300

    implementacao dos models

---
+++ java-actor-test/src/main/java/com/example/java_actor_test/user/User.java
@@ -0,0 +1,17 @@
+package com.example.java_actor_test.user;
+
+public class User {
+    private String id;
+
+    public User(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ java-actor-test/src/main/java/com/example/java_actor_test/user/UserToken.java
@@ -0,0 +1,17 @@
+package com.example.java_actor_test.user;
+
+public class UserToken {
+    private String token;
+
+    public UserToken(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
