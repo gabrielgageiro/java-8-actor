67b14b1e7e54ecb4a86291ed13ef8c0ecb731374
gabrielgageiro <gabrielgageiro@gmail.com>
2024-08-07 17:49:28 -0300

    iinit

---
+++ java-actor-test/src/main/java/com/example/java_actor_test/JavaActorTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_actor_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaActorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaActorTestApplication.class, args);
+	}
+
+}
